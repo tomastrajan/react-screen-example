bda05151a451901f68047c915aa0e8cd41856c26
Tomas Trajan <tomas.trajan@gmail.com>
2015-12-29 21:02:14 +0100

    feat(configuration): rework model into class, add update & remove methods

--- src/configuration/configuration-model-test.ts
+++ src/configuration/configuration-model-test.ts
@@ -2,11 +2,17 @@
 
 import { assert } from 'chai';
 
-import * as model from './configuration-model';
+import ConfigurationModel from './configuration-model';
 
 import { Configuration } from './configuration-interface';
 
+let model: ConfigurationModel;
+
 describe('Configuration Model', () => {
+
+    beforeEach(() => {
+        model = new ConfigurationModel();
+    });
 
     it('has empty default configurations', () => {
         assert.strictEqual(model.getConfigurations().length, 0);
@@ -24,6 +30,22 @@ describe('Configuration Model', () => {
 
         assert.strictEqual(model.getConfigurations().length, 1);
         assert.strictEqual(model.getConfigurations()[0].name, 'config');
+    });
+
+    it('updates configurations', () => {
+        let mockConfiguration: Configuration = {
+            name: 'config',
+            type: 'CSV',
+            scheduled: false,
+            recipients: [],
+            sql: 'SELECT * FROM trades'
+        };
+        model.addConfiguration(mockConfiguration);
+        mockConfiguration.name = 'changed';
+        model.updateConfiguration(mockConfiguration);
+
+        assert.strictEqual(model.getConfigurations().length, 1);
+        assert.strictEqual(model.getConfigurations()[0].name, 'changed');
     });
 
 });
--- src/configuration/configuration-model.ts
+++ src/configuration/configuration-model.ts
@@ -4,12 +4,34 @@ import * as _ from 'lodash';
 
 import { Configuration } from './configuration-interface.ts';
 
-const configurations: Configuration[] = [];
+export default class ConfigurationModel {
 
-export function getConfigurations(): Configuration[] {
-    return _.cloneDeep(configurations);
-}
+    private configurations: Configuration[];
 
-export function addConfiguration(configuration: Configuration): void {
-    configurations.push(_.cloneDeep(configuration));
+    constructor(initialConfigurations?: Configuration[]) {
+        this.configurations = initialConfigurations || [];
+    }
+
+    public getConfigurations(): Configuration[] {
+        return _.cloneDeep(this.configurations);
+    }
+
+    public addConfiguration(configuration: Configuration): void {
+        this.configurations.push(_.cloneDeep(configuration));
+    }
+
+    public updateConfiguration(configuration: Configuration): void {
+        _.forEach(this.configurations, (c: Configuration, index: number) => {
+            if (c.id === configuration.id) {
+                this.configurations[index] = configuration;
+            }
+        });
+    }
+
+    public removeConfiguration(id: number): void {
+        _.remove(this.configurations, (c: Configuration) => {
+            return c.id === id;
+        });
+    }
+
 }
